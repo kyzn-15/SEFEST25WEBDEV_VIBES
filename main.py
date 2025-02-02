e896846f3a353f8420a058cfb1f1b2646f9be349
winson <winsonlearn@gmail.com>
2025-02-02 19:04:25 +0700

    layout setup

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/homepage')
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
